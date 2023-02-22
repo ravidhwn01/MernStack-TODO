e9cd7bb02cfac2fae56597cc14c9fac98386c255
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-02-22 22:58:32 +0530

    data types complete

--- dataypeAndObjArr.js
+++ dataypeAndObjArr.js
@@ -1,3 +1,42 @@
+'use strict'
 // data types
 const data = null
+console.log(data);
+console.log(typeof null);
+
+const undef = undefined
+console.log(undef);
+
+
+console.log(null==undefined);
 console.log(null===undefined);
+
+// object
+const objct = {
+    profession :"developer",
+    age : 25,
+    city:"jaipur"
+}
+console.log(objct);
+
+
+// array
+const arr = [1,2,3,4,5,6,7,8,9,10]
+console.log(arr);
+
+const  arr1 = ["kalu", "shayam",4,5]
+console.log(arr1);
+
+// number 
+const num = 100;
+console.log(num);
+
+//boolean 
+const bool = true
+console.log(bool);
+console.log(typeof bool);
+
+
+const str = "this  is a string"
+console.log(str);
+console.log(typeof str);
