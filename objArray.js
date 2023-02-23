498bc0b08df9fbb609c188626caf8ccacbd2c4e3
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-02-23 08:12:35 +0530

    objects

---
+++ objArray.js
@@ -0,0 +1,16 @@
+//object
+'use strict';
+const car = {
+    carName :"rolls royce",
+    model:"Ghost",
+    fuelType :"petrol",
+    color:"blue",
+    price: 500000000
+
+}
+
+console.log(car.price);
+console.log(car.color);
+
+car.color = "black"
+console.log(car.color);
